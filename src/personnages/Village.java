e731fd6b1256a56c6c2ae9e905f1ee030478cba1
ludwig corentin <ldc4248a@ens.fsi>
2022-10-05 15:08:33 +0200

    tp

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -23,13 +23,28 @@ public class Village {
 	public void ajouteHabitant(Gaulois gaulois) {
 		if (nbvillageois < nbVillageoisMaximum) {
 			villageois[nbvillageois] = gaulois;
+			nbvillageois++;
 		}
-		nbvillageois++;
 
 	}
-	
-	public Gaulois trouvervillageois(int nvillageois) {
+
+	public Gaulois trouverHabitant(int nvillageois) {
 		return villageois[nvillageois];
 	}
 
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles", 30);
+//		Gaulois gaulois = village.trouverHabitant(30);
+//		Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 30
+//		at personnages.Village.trouverHabitant(Village.java:32)
+//		at personnages.Village.main(Village.java:37)
+//		Un tableau de 30 entre vas de 0 a 29
+		Chef Abraracourcix = new Chef("Abraracourcix", 6, village);
+		village.setChef(Abraracourcix);
+		Gaulois Asterix = new Gaulois("Asterix", 8);
+		village.ajouteHabitant(Asterix);
+		Gaulois gaulois = village.trouverHabitant(1);
+		System.out.println(gaulois);
+
+	}
 }
